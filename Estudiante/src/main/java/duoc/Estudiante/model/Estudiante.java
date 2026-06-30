b384fca08fdf78def22c099b1f72811ad2a93cb9
Jose <jos.rojo@duocuc.cl>
2026-06-30 16:47:11 -0400

    Proyecto actualizado

--- Estudiante/src/main/java/duoc/Estudiante/model/Estudiante.java
+++ Estudiante/src/main/java/duoc/Estudiante/model/Estudiante.java
@@ -26,6 +26,6 @@ public class Estudiante {
     @Column(nullable = false)
     private String email;
 
-    @Column(name = "carera_id", nullable = false)
+    @Column(name = "carrera_id", nullable = false)
     private Long CarreraId;
 }
